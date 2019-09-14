f3b8127e1eb25e61a5723a5cabe2fe37a96aed53
myTerminal <team.terminal@gmail.com>
2019-09-14 16:56:00 +0530

    tweak encryption filenames

--- src/re-write.js
+++ src/re-write.js
@@ -32,25 +32,8 @@ const getHexFromByte = input => (`0${input.toString(16)}`).slice(-2);
 // Function to convert hex to byte
 const getByteFromHex = input => parseInt(input, 16);
 
-// Function to determine common directory between input file paths
-const getBaseDirectory = filePaths => {
-    const firstPath = path.dirname(filePaths[0]),
-        segments = firstPath.split('/'),
-        pathsToTry = new Array(segments).fill('').map((n, i) => segments.slice(0, i + 1).join('/')),
-        baseDirectories = pathsToTry.filter(
-            p => filePaths.filter(f => f.indexOf(p) === 0).length === filePaths.length
-        ).reverse();
-
-    return baseDirectories[0] || '.';
-};
-
-// Function to remove '..' from paths
-const getLiftedFilePaths = filePaths =>
-    filePaths.map(p => p.split('/').filter(s => s !== '..').join('/'));
-
-// Function to file paths relative to a base directory
-const getRelativePaths = (filePaths, baseDirectory) =>
-    filePaths.map(p => path.relative(baseDirectory, p));
+// Function to lift paths
+const liftPath = path => path.split('/').filter(s => s !== '..').join('/');
 
 // Function to get absolute file paths for an output directory
 const getFinalFilePaths = (filePaths, outputDirectoryPath) =>
@@ -75,14 +58,14 @@ module.exports.doIt = (inputFilePaths, outputFilePath) => {
     const password = prompt.question('Enter a password if you want to use one: ', { hideEchoBack: true });
     const metadata = `${latestTransformIndex},${(password && md5(password))}`;
     const textFromFiles = inputFilePaths.map(
-        f => f + delimiters.data + transform(fs.readFileSync(f))
+        f => `${liftPath(f)}${delimiters.data}${transform(fs.readFileSync(f))}`
     ).join(delimiters.files);
     const encryptedText = encryptOrDecryptText(textFromFiles, password);
 
     io.showMessage(inputFilePaths.length, 'input files provided');
 
     // Do the final re-write
-    fs.writeFileSync(outputFilePath, metadata + delimiters.main + encryptedText);
+    fs.writeFileSync(outputFilePath, `${metadata}${delimiters.main}${encryptedText}`);
 
     io.showMessage('Data re-written at', outputFilePath);
 };
@@ -112,10 +95,7 @@ module.exports.undoIt = (inputFilePath, outputDirectoryPath) => {
         }
     );
     const fetchedFilePaths = outputFilesData.map(d => d.name);
-    const baseDirectory = getBaseDirectory(fetchedFilePaths);
-    const liftedFilePaths = getLiftedFilePaths(fetchedFilePaths);
-    const relativeFilePaths = getRelativePaths(liftedFilePaths, baseDirectory);
-    const finalFilePaths = getFinalFilePaths(relativeFilePaths, outputDirectoryPath);
+    const finalFilePaths = getFinalFilePaths(fetchedFilePaths, outputDirectoryPath);
 
     io.showMessage(finalFilePaths.length, 'files to be (un)re-written');
 
